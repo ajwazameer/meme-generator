659c0ac44e7c02d431d77bb570a1d2c7654da46d
ajwazameer <ajwazameer2004@gmail.com>
2026-07-28 19:26:55 +0500

    Implement: Added Api and feature to get new image button

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,66 @@
+import { useState, useEffect } from "react";
 export default function Main() {
+  const URL = "https://api.imgflip.com/get_memes";
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+  function handleChange(event) {
+    const { value, name } = event.currentTarget;
+    setMeme((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
+    console.log(meme.topText);
+  }
+  const [memeArray, setMemeArray] = useState([]);
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => setMemeArray(data.data.memes));
+  }, []);
+
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * memeArray.length);
+  }
+  function getNewMeme() {
+    const num = generateRandomNumber();
+    setMeme((prevValue) => ({
+      ...prevValue,
+      imageUrl: memeArray[num].url,
+    }));
+  }
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not simply" name="topText" />
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handleChange}
+            value={meme.topText}
+          />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            onChange={handleChange}
+            value={meme.bottomText}
+          />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button onClick={getNewMeme}>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   );
